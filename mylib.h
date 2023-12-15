3a1fb82eec35524b77919f47279f6e71b454f071
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-12-15 16:03:33 +0200

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -103,167 +103,170 @@ private:
     float galutinisMedVectorRanka();
 };
 
-class Studentas {
+class Studentas : public Zmogus {
     private:
-       std::string vardas, pavarde;
-       std::vector<int> pazymiai;
-       int egzaminas;
-       float vidurkis, mediana, rezultatasv, rezultatasm;
+    std::string vardas, pavarde;
+    std::vector<int> pazymiai;
+    int egzaminas;
+    float vidurkis, mediana, rezultatasv, rezultatasm;
 
     public:
-       Studentas(const std::string& vardas ="", const std::string& pavarde = "", const std::vector<int>& pazymiai = {}, int egzaminas = 0)
-       : vardas(vardas), pavarde(pavarde), pazymiai(pazymiai), egzaminas(egzaminas),
-         rezultatasv(galutinisVidVector()), rezultatasm(galutinisMedVector())
-          { std::cout << "Parametrizuotas konstruktorius iskviestas\n";}
-   
-       Studentas(const Studentas& other)
-           : vardas(other.vardas), pavarde(other.pavarde), pazymiai(other.pazymiai), egzaminas(other.egzaminas),
-             vidurkis(other.vidurkis), mediana(other.mediana), rezultatasv(other.rezultatasv), rezultatasm(other.rezultatasm) {
-             std::cout << "Kopijavimo konstruktorius iskviestas " << vardas << " " << pavarde << "\n"; }
-   
-       Studentas& operator=(const Studentas& other) {
-           if (this != &other) {
-               vardas = other.vardas;
-               pavarde = other.pavarde;
-               pazymiai = other.pazymiai;
-               egzaminas = other.egzaminas;
-               vidurkis = other.vidurkis;
-               mediana = other.mediana;
-               rezultatasv = other.rezultatasv;
-               rezultatasm = other.rezultatasm;
-           }
-           std::cout<<"Kopijavimo priskirties operatorius iskviestas " << vardas << " " << pavarde << "\n";
-           return *this;
-       }
-   
-          const std::string& getVardas() const {
-              return vardas;
-          }
-          const std::string& getPavarde() const {
-              return pavarde;
-          }
-          const std::vector<int>& getPazymiai() const {
-              return pazymiai;
-          }
-          int getEgzaminas() const {
-              return egzaminas;
-          }
-          float getRezultatasV()  const {
-              return rezultatasv;
-          }
-          float getRezultatasM() const {
-              return rezultatasm;
-          }
-      
-          void setVardas(const std::string& vardas) {
-              this->vardas = vardas;
-          }
-          void setPavarde(const std::string& pavarde) {
-              this->pavarde = pavarde;
-          }
-          void setPazymiai(const std::vector<int>& pazymiai) {
-              this->pazymiai = pazymiai;
-          }
-          void setEgzaminas(int egzaminas) {
-              this->egzaminas = egzaminas;
-          }
-          void setVidurkis(float vidurkis) {
-              this->vidurkis = vidurkis;
-          }
-          void setMediana(float mediana) {
-              this->mediana = mediana;
-          }
-          void setRezultatasV(float rezultatasv) {
-              this->rezultatasv = rezultatasv;
-          }
-          void setRezultatasM(float rezultatasm) {
-              this->rezultatasm = rezultatasm;
-          }
-   
-       ~Studentas(){std::cout<<"Iskviestas destruktorius " << vardas << " " << pavarde << "\n";};
+    Studentas(const std::string& vardas ="", const std::string& pavarde = "", const std::vector<int>& pazymiai = {}, int egzaminas = 0)
+    : vardas(vardas), pavarde(pavarde), pazymiai(pazymiai), egzaminas(egzaminas),
+      rezultatasv(galutinisVidVector()), rezultatasm(galutinisMedVector())
+ {
+        std::cout << "Parametrizuotas konstruktorius iskviestas\n";
+}
+
+    Studentas(const Studentas& other)
+        : vardas(other.vardas), pavarde(other.pavarde), pazymiai(other.pazymiai), egzaminas(other.egzaminas),
+          vidurkis(other.vidurkis), mediana(other.mediana), rezultatasv(other.rezultatasv), rezultatasm(other.rezultatasm) {
+          std::cout << "Kopijavimo konstruktorius iskviestas " << vardas << " " << pavarde << "\n"; }
+
+    Studentas& operator=(const Studentas& other) {
+        if (this != &other) {
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            pazymiai = other.pazymiai;
+            egzaminas = other.egzaminas;
+            vidurkis = other.vidurkis;
+            mediana = other.mediana;
+            rezultatasv = other.rezultatasv;
+            rezultatasm = other.rezultatasm;
+        }
+        std::cout<<"Kopijavimo priskirties operatorius iskviestas " << vardas << " " << pavarde << "\n";
+        return *this;
+    }
+
+    std::string getVardas() const override {
+        return vardas;
+    }
+    std::string getPavarde() const override {
+        return pavarde;
+    }
+    const std::vector<int>& getPazymiai() const {
+        return pazymiai;
+    }
+    int getEgzaminas() const {
+        return egzaminas;
+    }
+    float getRezultatasV()  const {
+        return rezultatasv;
+    }
+    float getRezultatasM() const {
+        return rezultatasm;
+    }
+
+    void setVardas(const std::string& vardas) {
+        this->vardas = vardas;
+    }
+    void setPavarde(const std::string& pavarde) {
+        this->pavarde = pavarde;
+    }
+    void setPazymiai(const std::vector<int>& pazymiai) {
+        this->pazymiai = pazymiai;
+    }
+    void setEgzaminas(int egzaminas) {
+        this->egzaminas = egzaminas;
+    }
+    void setVidurkis(float vidurkis) {
+        this->vidurkis = vidurkis;
+    }
+    void setMediana(float mediana) {
+        this->mediana = mediana;
+    }
+    void setRezultatasV(float rezultatasv) {
+        this->rezultatasv = rezultatasv;
+    }
+    void setRezultatasM(float rezultatasm) {
+        this->rezultatasm = rezultatasm;
+    }
+
+    ~Studentas(){std::cout<<"Iskviestas destruktorius " << vardas << " " << pavarde << "\n";};
 
 private:
     float galutinisVidVector() const {return 0; };
     float galutinisMedVector() const {return 0; };
 };
 
-class Studentas2 {
+class Studentas2 : public Zmogus {
     private:
-       std::string vardas, pavarde;
-       std::list<int> pazymiai;
-       int egzaminas;
-       float vidurkis, mediana, rezultatasv, rezultatasm;
+    std::string vardas, pavarde;
+    std::list<int> pazymiai;
+    int egzaminas;
+    float vidurkis, mediana, rezultatasv, rezultatasm;
 
     public:
-       Studentas2(const std::string& vardas ="", const std::string& pavarde = "", const std::list<int>& pazymiai = {}, int egzaminas = 0)
-       : vardas(vardas), pavarde(pavarde), pazymiai(pazymiai), egzaminas(egzaminas),
-         rezultatasv(galutinisVidList()), rezultatasm(galutinisMedList()) {}
-   
-       Studentas2(const Studentas2& other)
-           : vardas(other.vardas), pavarde(other.pavarde), pazymiai(other.pazymiai), egzaminas(other.egzaminas),
-             vidurkis(other.vidurkis), mediana(other.mediana), rezultatasv(other.rezultatasv), rezultatasm(other.rezultatasm) {
-             std::cout << "Kopijavimo konstruktorius iskviestas " << vardas << " " << pavarde << "\n"; }
-   
-       Studentas2& operator=(const Studentas2& other) {
-           if (this != &other) {
-               vardas = other.vardas;
-               pavarde = other.pavarde;
-               pazymiai = other.pazymiai;
-               egzaminas = other.egzaminas;
-               vidurkis = other.vidurkis;
-               mediana = other.mediana;
-               rezultatasv = other.rezultatasv;
-               rezultatasm = other.rezultatasm;
-           }
-           std::cout<<"Kopijavimo priskirties operatorius iskviestas " << vardas << " " << pavarde << "\n";
-           return *this;
-       }
-   
-          const std::string& getVardas() const {
-              return vardas;
-          }
-          const std::string& getPavarde() const {
-              return pavarde;
-          }
-          const std::list<int>& getPazymiai() const {
-              return pazymiai;
-          }
-          int getEgzaminas() const {
-              return egzaminas;
-          }
-          float getRezultatasV()  const {
-              return rezultatasv;
-          }
-          float getRezultatasM() const {
-              return rezultatasm;
-          }
-      
-          void setVardas(const std::string& vardas) {
-              this->vardas = vardas;
-          }
-          void setPavarde(const std::string& pavarde) {
-              this->pavarde = pavarde;
-          }
-          void setPazymiai(const std::list<int>& pazymiai) {
-              this->pazymiai = pazymiai;
-          }
-          void setEgzaminas(int egzaminas) {
-              this->egzaminas = egzaminas;
-          }
-          void setVidurkis(float vidurkis) {
-              this->vidurkis = vidurkis;
-          }
-          void setMediana(float mediana) {
-              this->mediana = mediana;
-          }
-          void setRezultatasV(float rezultatasv) {
-              this->rezultatasv = rezultatasv;
-          }
-          void setRezultatasM(float rezultatasm) {
-              this->rezultatasm = rezultatasm;
-          }
-   
-       ~Studentas2(){std::cout<<"Iskviestas destruktorius " << vardas << " " << pavarde << "\n";};
+    Studentas2(const std::string& vardas ="", const std::string& pavarde = "", const std::list<int>& pazymiai = {}, int egzaminas = 0)
+    : vardas(vardas), pavarde(pavarde), pazymiai(pazymiai), egzaminas(egzaminas),
+      rezultatasv(galutinisVidList()), rezultatasm(galutinisMedList())
+ {
+}
+    Studentas2(const Studentas2& other)
+        : vardas(other.vardas), pavarde(other.pavarde), pazymiai(other.pazymiai), egzaminas(other.egzaminas),
+          vidurkis(other.vidurkis), mediana(other.mediana), rezultatasv(other.rezultatasv), rezultatasm(other.rezultatasm) {
+          std::cout << "Kopijavimo konstruktorius iskviestas " << vardas << " " << pavarde << "\n"; }
+
+    Studentas2& operator=(const Studentas2& other) {
+        if (this != &other) {
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            pazymiai = other.pazymiai;
+            egzaminas = other.egzaminas;
+            vidurkis = other.vidurkis;
+            mediana = other.mediana;
+            rezultatasv = other.rezultatasv;
+            rezultatasm = other.rezultatasm;
+        }
+        std::cout<<"Kopijavimo priskirties operatorius iskviestas " << vardas << " " << pavarde << "\n";
+        return *this;
+    }
+
+    std::string getVardas() const override {
+        return vardas;
+    }
+    std::string getPavarde() const override {
+        return pavarde;
+    }
+    const std::list<int>& getPazymiai() const {
+        return pazymiai;
+    }
+    int getEgzaminas() const {
+        return egzaminas;
+    }
+    float getRezultatasV()  const {
+        return rezultatasv;
+    }
+    float getRezultatasM() const {
+        return rezultatasm;
+    }
+
+    void setVardas(const std::string& vardas) {
+        this->vardas = vardas;
+    }
+    void setPavarde(const std::string& pavarde) {
+        this->pavarde = pavarde;
+    }
+    void setPazymiai(const std::list<int>& pazymiai) {
+        this->pazymiai = pazymiai;
+    }
+    void setEgzaminas(int egzaminas) {
+        this->egzaminas = egzaminas;
+    }
+    void setVidurkis(float vidurkis) {
+        this->vidurkis = vidurkis;
+    }
+    void setMediana(float mediana) {
+        this->mediana = mediana;
+    }
+    void setRezultatasV(float rezultatasv) {
+        this->rezultatasv = rezultatasv;
+    }
+    void setRezultatasM(float rezultatasm) {
+        this->rezultatasm = rezultatasm;
+    }
+
+    ~Studentas2(){std::cout<<"Iskviestas destruktorius " << vardas << " " << pavarde << "\n";};
 
 
 private:
