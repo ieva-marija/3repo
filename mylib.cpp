816f0e22974b139ef4d426dca7b62c5ce852f5bb
ieva-marija <144428382+ieva-marija@users.noreply.github.com>
2023-12-27 14:26:03 +0200

    Update mylib.cpp

--- mylib.cpp
+++ mylib.cpp
@@ -10,6 +10,76 @@
 
 using namespace std;
 
+StudentasRanka ivesk()
+{
+  StudentasRanka temp;
+  cout << "Iveskite varda: ";
+  cin >> temp.getVardas();
+  cout << "Iveskite pavarde: ";
+  cin >> temp.getPavarde();
+
+  int pazymys;
+  int pasirinkimas2;
+  cout << "Ar norite, kad programa studento pazymius ir egzamino rezultata generuotu atsitiktinai?" << endl;
+  cout << "5 - taip, noriu, aciu" << endl;
+  cout << "9 - ne, noriu ivesti savarankiskai" << endl;
+    cin >> pasirinkimas2;
+
+    if (pasirinkimas2 == 5) {
+        srand(time(0));
+        int a_pazymiai_sk = rand() % 11;
+        cout << "Sugeneruoti atsitiktinai pazymiai: ";
+        for (int i = 0; i < a_pazymiai_sk; ++i) {
+            int a_pazymys = rand() % 11 + 1;
+            temp.getPazymiai().push_back(a_pazymys);
+            cout << a_pazymys << " ";
+        }
+
+        srand(time(0));
+        temp.setEgzaminas(rand() % 11 + 1);
+        cout << "Sugeneruotas egzamino pazymys: "<< temp.getEgzaminas() << endl;
+        cout << "\n";
+    }
+     else if (pasirinkimas2 == 9) {
+       while (true) {
+            cout << "Iveskite namu darbu rezultatus (iveskite -1, kai suvedete visus norimus pazymius): ";
+            if (cin >> pazymys) {
+                if (pazymys == -1) {
+                    cin.clear();
+                    cin.ignore(256, '\n');
+                    break;
+                }
+                if (pazymys >= 0 && pazymys <= 10) {
+                    temp.getPazymiai().push_back(pazymys);
+                }
+                else {
+                    cout << "??????????" << endl;
+                    }
+            }
+            else {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "????????????" << endl;
+                }
+       }
+       cout << "Iveskite egzamino pazymi: ";
+       int egzaminas;
+        cin >> egzaminas;
+        temp.setEgzaminas(egzaminas);
+        while (temp.getEgzaminas() > 10) {
+            cout << "MELAGIS. VESK IS NAUJO";
+            cin >> egzaminas;
+            temp.setEgzaminas(egzaminas);
+        }
+     }
+     else {
+        cout << "nu cia nei 5, nei 9........" << endl;
+        return temp;
+     }
+     temp.setRezultatasV(galutinisVidVectorRanka(temp));
+     temp.setRezultatasM(galutinisMedVectorRanka(temp));
+  return temp;
+};
 //-------------------------------------------------------
 float galutinisVidVector(const Studentas& studentas) {
     float vidurkis = 0;
@@ -66,6 +136,30 @@ void rusiavimasVector(vector<Studentas>& studentai) {
 void rusiavimasList(list<Studentas2>& studentai2) {
     studentai2.sort([](const Studentas2& a, const Studentas2& b) {
         return a.getPavarde() < b.getPavarde();
+    });
+};
+//----------------------------------------------------------
+void rusiavimasVector2(vector<Studentas>& studentai) {
+    std::sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
+        return a.getVardas() > b.getVardas();
+    });
+};
+//----------------------------------------------------------
+void rusiavimasList2(list<Studentas2>& studentai2) {
+    studentai2.sort([](const Studentas2& a, const Studentas2& b) {
+        return a.getVardas() < b.getVardas();
+    });
+};
+//----------------------------------------------------------
+void rusiavimasVector3(vector<Studentas>& studentai) {
+    std::sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
+        return a.getRezultatasV() < b.getRezultatasV();
+    });
+};
+//----------------------------------------------------------
+void rusiavimasList3(list<Studentas2>& studentai2) {
+    studentai2.sort([](const Studentas2& a, const Studentas2& b) {
+        return a.getRezultatasV() < b.getRezultatasV();
     });
 };
 //-------------------------------------------------------
@@ -164,5 +258,3 @@ std::istream& operator>>(std::istream& is, StudentasRanka& student) {
     student.setRezultatasM(galutinisMedVectorRanka(student));
     return is;
 }
-
-
